d68cf7e3be2a3b1889856902339a84fe44cf810a
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-11-02 17:36:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Min_Max_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = Console.ReadLine();
+            int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+            
+            int N = int.Parse(Console.ReadLine());
+
+            if (N <= numbers.Length)
+            {
+                int max = numbers.Take(N).Max();
+                int min = numbers.Take(N).Min();
+
+                Console.WriteLine(max);
+                Console.WriteLine(min);
+            }
+            else
+            {
+                Console.WriteLine("N is greater than the number of elements in the array.");
+            }
+        }
+    }
+}
